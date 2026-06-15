ee3b792037e3e4cd787de6cda2149b176be8822b
Gyanti Kumari <kgyanti54@gmail.com>
2026-06-15 11:55:06 +0000

    var

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,8 @@
+const accountId="49577393"
+let accountEmail="kgyanti@gmail.com"
+var accountPassword="1234"
+accountCity="Ranchi"
+accountEmail = "heuen@gmail.com"
+accountPassword="8499949"
+acountCity="gujrat"
+comsole.table([accontEmail,accontId,accountPassword])
